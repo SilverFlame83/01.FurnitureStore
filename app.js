f54881d5ad02e3ce2608a0b98f0c12a474bf9dd4
VelvetSyntax <imren_83@abv.bg>
2024-01-29 18:25:14 +0200

    onAdd function implemented

--- app.js
+++ app.js
@@ -1,5 +1,49 @@
-window.addEventListener('load', solve);
+window.addEventListener("load", solve);
 
 function solve() {
-    //TO DO
+  const modelInput = document.getElementById("model");
+  const yearInput = document.getElementById("year");
+  const descriptionInput = document.getElementById("description");
+  const price = document.getElementById("price");
+  const furnitureList = document.getElementById("furniture-list");
+  const addBtn = document.getElementById("add");
+
+  addBtn.addEventListener("click", onAdd);
+
+  function onAdd() {
+    let priceNum = Number(price.value);
+    if (
+      modelInput.value === "" ||
+      yearInput.value === "" ||
+      descriptionInput.value === "" ||
+      priceNum > 0
+    ) {
+      return;
+    }
+
+    console.log(
+      modelInput.value,
+      yearInput.value,
+      descriptionInput.value,
+      priceNum
+    );
+    function elManager(type, txtContent, className) {
+      const el = document.createElement(type);
+      if (txtContent) {
+        el.textContent = txtContent;
+      }
+      if (className) {
+        el.className = className;
+      }
+
+      return el;
+    }
+
+    function append(main, ...arg) {
+      while (arg.length) {
+        main.appendChild(arg.shift());
+      }
+      return main;
+    }
+  }
 }
